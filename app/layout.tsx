4e32adb9de088ea790efcb6c43c2cccd76d391e6
Arshita Navin <navinarshita443@gmail.com>
2026-04-03 14:00:49 +0530

    Fixed layout duplication and removed analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Script from "next/script";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -18,34 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className="antialiased bg-slate-50 flex flex-col min-h-screen">
-        {/* Navbar hamesha top par rahega kyunki wo fixed hai */}
-        <Navbar />
-
-        {/* Main content ko 'flex-grow' diya hai taaki agar content kam ho 
-          toh bhi Footer hamesha screen ke bottom mein rahe.
-        */}
-
-        
-        <main className="flex-grow">
-          {children}
-        </main>
-
-        <Footer />
-
-
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-JSC5BDFZ3Z"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JSC5BDFZ3Z');
-          `}
-        </Script>
 
         <Navbar />
 
@@ -54,8 +25,8 @@ export default function RootLayout({
         </main>
 
         <Footer />
+
       </body>
-
     </html>
   );
 }
